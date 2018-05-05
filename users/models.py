94eb9743399c8373d7253b842c94657fcc58329c
Michelle <33115449+micxmusic@users.noreply.github.com>
2018-05-05 16:31:06 +1000

    users class defined

--- users/models.py
+++ users/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class UserManager(models.Manager):
     def create_user(self, name, university, address, email, password):
-        user = self.create(name = name, university = university, address = address, email = email, password = password)
+        user = self.create(name = name, university = university, address = address,
+                           email = email, password = password)
         return user
 
 class User(models.Model):
@@ -14,5 +15,6 @@ class User(models.Model):
     address = models.CharField(max_length= 100)
     email = models.EmailField(max_length= 255)
     password = models.CharField(max_length = 50)
+    credit = models.IntegerField(default= 100)
 
     objects = UserManager
