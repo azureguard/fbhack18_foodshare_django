c1564c1cf43c929586e537711133e2afbfaa7d77
Daryl Wong <azureguard@users.noreply.github.com>
2018-05-05 19:05:46 +1000

    extend sharing models and templates

--- sharing/models.py
+++ sharing/models.py
@@ -1,17 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
-class ItemManager(models.Manager):
-    def create_item(self, name, category, amount, unit):
-        item = self.create(name=name, type=category, amount=amount, unit=unit)
-        return item
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    university = models.TextField(max_length=500, blank=True)
+    address = models.CharField(max_length=1000, blank=True)
 
 
-class RecipeManager(models.Manager):
-    def create_recipe(self, name):
-        recipe = self.create(user=User.first_name, name=name, items=Item.name)
-        return recipe
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+# class ItemManager(models.Manager):
+#     def create_item(self, name, category, amount, unit):
+#         item = self.create(name=name, type=category, amount=amount, unit=unit)
+#         return item
+
+
+# class RecipeManager(models.Manager):
+#     def create_recipe(self, name):
+#         recipe = self.create(user=User.first_name, name=name, items=Item.name)
+#         return recipe
 
 
 class AvailableItemManager(models.Manager):
@@ -28,12 +47,12 @@ class Item(models.Model):
     unit = models.CharField(max_length=2, choices=UNIT_CHOICES)
 
 
-class Recipe(models.Model):
-    user = models.ForeignKey('User', on_delete=models.PROTECT)
-    name = models.CharField(max_length=50)
-    items = models.ManyToManyField('Item')
+# class Recipe(models.Model):
+#     user = models.ForeignKey('User', on_delete=models.PROTECT)
+#     name = models.CharField(max_length=50)
+#     items = models.ManyToManyField('Item')
 
 
-class AvailableItem(models.Model):
-    item = models.ForeignKey('Item', on_delete=models.PROTECT)
-    user = models.ForeignKey('User', on_delete=models.PROTECT)
+# class AvailableItem(models.Model):
+#     item = models.ForeignKey('Item', on_delete=models.PROTECT)
+#     user = models.ForeignKey('User', on_delete=models.PROTECT)
--- sharing/views.py
+++ sharing/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from sharing.forms import UserForm, ProfileForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from .models import User, Profile
+
 
 # Create your views here.
+
+def user_details(request):
+    user = get_object_or_404(User, pk=id)
+    profile = Profile.objects.get()
+    return render(request, 'sharing/details.html', {'user': user})
+
+
+@login_required
+@transaction.atomic
+def update_profile(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request,
+                             _('Your profile was successfully updated!'))
+            return redirect('settings:profile')
+        else:
+            messages.error(request, _('Please correct the error below.'))
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'sharing/profile.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
+
+
+def index(request):
+    return render(request, 'sharing/index.html')
