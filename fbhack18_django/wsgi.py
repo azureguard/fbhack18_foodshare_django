db1d5b8bfa86e22dfd85a7f06fe408ccab100657
Daryl Wong <azureguard@users.noreply.github.com>
2018-05-05 18:07:50 +1000

    rebase

--- fbhack18_django/wsgi.py
+++ fbhack18_django/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for fbhack_django project.
+WSGI config for fbhack18_django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fbhack_django.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fbhack18_django.settings")
 
 application = get_wsgi_application()
